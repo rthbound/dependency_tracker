0cf678f53028b6ef413e6a79ad78b142905d2679
Tad Hosford <tad.hosford@gmail.com>
2013-03-19 20:13:25 -0500

    Create the current GEM_HOME if it's defined. README reorganization.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Project.create(name: "NV Medicaid", gem_home: "/home/rt/.rvm/gems/ruby-1.9.3-p392@nv_medicaid_mri")
-Project.create(name: "NV Medicare", gem_home: "/home/rt/.rvm/gems/ruby-1.9.3-p374@nv_medicare_mri")
-Project.create(name: "Well Portal", gem_home: "/home/rt/.rvm/gems/ruby-1.9.3-p392@WellPortal_mri")
-Project.create(name: "Trackers", gem_home: "/home/rt/.rvm/gems/jruby-1.7.0.RC1@Trackers")
-Project.create(name: "Cardholder", gem_home: "/home/rt/.rvm/gems/jruby-1.7.3@Cardholder")
-Project.create(name: "CATS", gem_home: "/home/rt/.rvm/gems/jruby-1.7.0.RC1@CATS")
-Project.create(name: "Cheetah Cub", gem_home: "/home/rt/.rvm/gems/ruby-1.9.3-p392@cheetah_cub")
-Project.create(name: "PatientManager Engine", gem_home: "/home/rt/.rvm/gems/jruby-1.7.0.RC1@rails_engines")
+if defined? ENV['GEM_HOME']
+  Project.create(name: 'current $GEM_HOME', ENV['GEM_HOME'])
+end
